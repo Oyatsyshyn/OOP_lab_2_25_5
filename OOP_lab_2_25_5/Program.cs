14445bb30419b8510a01620e5a16453d55389717
Роман Данилевич <dwordchanel@gmail.com>
2020-05-26 19:45:22 +0300

    Add project files.

---
+++ OOP_lab_2_25_5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OOP_lab_2_25_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] a = new int[n, m];
+
+            var rnd = new Random();
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    a[i, j] = rnd.Next(-100, 100);
+                }
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write("{0, -6} ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < n; ++i)
+            {
+                bool isPositive = false;
+                int d = 1;                
+
+                for (int j = 0; j < m; ++j)
+                {
+                    if (a[i, j] > 0)
+                    {
+                        isPositive = true;
+
+                        d *= a[i, j];
+                    }
+                }
+
+                if (isPositive)
+                {
+                    Console.WriteLine(d);
+                }
+            }
+        }
+    }
+}
